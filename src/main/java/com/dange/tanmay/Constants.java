bfc8a6a23449c38b5c98dfc632414f34888c81dd
tanmay <quest_svit@yahoo.co.in>
2020-07-05 15:07:22 +0530

    Adding Feature:View logs by HEAD and TAIL with specific number of lines

---
+++ src/main/java/com/dange/tanmay/Constants.java
@@ -0,0 +1,6 @@
+package com.dange.tanmay;
+
+public interface Constants {
+	String DIRECTION_HEAD="HEAD";
+	String DIRECTION_TAIL="TAIL";
+}
--- src/main/java/com/dange/tanmay/controllers/LogViewController.java
+++ src/main/java/com/dange/tanmay/controllers/LogViewController.java
@@ -1,11 +1,19 @@
 package com.dange.tanmay.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dange.tanmay.Constants;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -15,37 +23,46 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-@Controller
+@RestController
 public class LogViewController {
 	
 	
 	
 	@RequestMapping(method = RequestMethod.GET, value="/viewLog")
 	@ResponseBody
-	public StringBuffer  viewLog(@RequestParam String filePath) {
+	public StringBuffer  viewLog(@RequestParam int lines,@RequestParam String filePath,@RequestParam  String direction) {
 		
 		BufferedReader reader;
 		StringBuffer strbuf= new StringBuffer();
 		
-		try {
-			
-			
+		try {	
 			reader = new BufferedReader(new FileReader(filePath));
-			String line = reader.readLine();
-			while (line != null) {
-				//System.out.println(line);
-				strbuf.append("<br>"+line);
-				// read next line
-				line = reader.readLine();
+			direction = Objects.nonNull(direction)? direction : Constants.DIRECTION_TAIL;
+			
+			if (Constants.DIRECTION_HEAD.equals(direction)) {
+				int counter=0;
+				String line = reader.readLine();
+				while (counter < lines) {
+					strbuf.append("<br>"+reader.readLine());
+					counter++;
+				}
+			}else {
+				int counter=0;
+				@SuppressWarnings("deprecation")
+				ReversedLinesFileReader object = new ReversedLinesFileReader(new File(filePath));
+				List<String> reversedLines = new ArrayList<>();
+				while (counter < lines) {
+					reversedLines.add("<br>"+reader.readLine());
+					counter++;
+				}
+				for (int i=reversedLines.size()-1; i>=0;i--) {
+					strbuf.append(reversedLines.get(i));
+				}
 			}
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	return strbuf;
 	}
-	
-
 }
