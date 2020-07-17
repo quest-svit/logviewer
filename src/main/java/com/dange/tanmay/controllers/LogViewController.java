c7d5cbad11f6165cdb8a2cb56c000ae2b2341794
tanmay <quest_svit@yahoo.co.in>
2020-07-17 18:39:33 +0530

    Adding LogViewing by number of lines and Head and Tail

--- src/main/java/com/dange/tanmay/controllers/LogViewController.java
+++ src/main/java/com/dange/tanmay/controllers/LogViewController.java
@@ -3,28 +3,24 @@ package com.dange.tanmay.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 import org.apache.commons.io.input.ReversedLinesFileReader;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dange.tanmay.Constants;
-
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class LogViewController {
+	
+	public static Logger log = LoggerFactory.getLogger(LogViewController.class);
 	
 	@RequestMapping(method = RequestMethod.GET, value="/viewLog")
 	@ResponseBody
@@ -32,19 +28,24 @@ public class LogViewController {
 		
 		BufferedReader reader;
 		StringBuffer strbuf= new StringBuffer();
+		log.info("FilePath: "+ filePath);
+		log.info("No of Lines to View: "+ lines);
+		log.info("Direction: "+ direction);
 		
 		try {	
 			reader = new BufferedReader(new FileReader(filePath));
 			direction = Objects.nonNull(direction)? direction : Constants.DIRECTION_TAIL;
 			
 			if (Constants.DIRECTION_HEAD.equals(direction)) {
+				log.debug("Direction:"+ direction);
 				int counter=0;
-				String line = reader.readLine();
+				reader.readLine();
 				while (counter < lines) {
 					strbuf.append("<br>"+reader.readLine());
 					counter++;
 				}
 			}else {
+				log.debug("Direction is TAIL");
 				int counter=0;
 				@SuppressWarnings("deprecation")
 				ReversedLinesFileReader object = new ReversedLinesFileReader(new File(filePath));
@@ -56,10 +57,11 @@ public class LogViewController {
 				for (int i=reversedLines.size()-1; i>=0;i--) {
 					strbuf.append(reversedLines.get(i));
 				}
+				object.close();
 			}
 			reader.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error("IO Exception occurred" , e);
 		}
 	return strbuf;
 	}
